7ddab3055c486f58ce30028c88265b88b08858b5
bakaluba edward <bakedie22@gmail.com>
2025-12-12 15:24:27 -0500

    Trigger redeploy

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,6 +1,5 @@
-
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yeagmfnhncefqjrepabk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllYWdtZm5obmNlZnFqcmVwYWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NjYwOTQsImV4cCI6MjA1NzM0MjA5NH0.38OlIrL9Q-bFCXAcKn1jkG6mC7StKIN2aMGoiaO8UZE'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
